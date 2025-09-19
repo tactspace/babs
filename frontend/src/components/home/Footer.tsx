e78b4f5756373a28007562281e75a355112d0dbe
Ashish Patel <ashishp16.2000@gmail.com>
2025-09-20 00:08:10 +0200

    Infra/update cicd (#10)
    * updated cicd fixed npm outdated packages
    
    * removed route groups, updated workflow

--- frontend/src/components/home/Footer.tsx
+++ frontend/src/components/home/Footer.tsx
@@ -1,4 +1,5 @@
-// Footer.tsx
+"use client";
+
 export default function Footer() {
     return (
       <footer className="border-t border-border py-4">
--- frontend/src/components/home/HowItWorks.tsx
+++ frontend/src/components/home/HowItWorks.tsx
@@ -1,4 +1,3 @@
-// HowItWorksSection.tsx
 "use client";
 
 import { useState, useEffect } from "react";
